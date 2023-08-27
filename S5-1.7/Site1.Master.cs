470618d053b001778cd93d588e44a1610c25305c
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-08-27 12:45:26 +0530

    first commit

---
+++ S5-1.7/Site1.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace S5_1._7
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
